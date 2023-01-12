14c6b92629390948868725b286beb083422baa53
Bishal Giri <bishal0922@gmail.com>
2023-01-12 17:37:25 -0600

    add delete

--- routes/users.js
+++ routes/users.js
@@ -5,16 +5,16 @@ import {v4 as uuidv4} from 'uuid';
 const router = express.Router();
 
 const users = [
-    {
-    name: "bishal0922",
-    last: "what is none",
-    age: 35
-    },
-    {
-    name: "bishal0923",
-    last: "what is none",
-    age: 35
-    },
+    // {
+    // name: "bishal0922",
+    // last: "what is none",
+    // age: 35
+    // },
+    // {
+    // name: "bishal0923",
+    // last: "what is none",
+    // age: 35
+    // },
 ]
 
 //we use the router to create a route
@@ -42,6 +42,25 @@ router.post('/', (req, res) => {
 
     users.push(userWithID);
     res.send(`The user ${user.name} was added to the database`)
+});
+
+//if its "/:id" then you're expecting anyting after the users path so the id doesn't really matter
+router.get('/:id', (req,res)=>{
+    //take the id value from req.params
+    const {id} = req.params;
+
+    const foundUser = users.find((user) => user.id === id);
+    res.send(foundUser)
+})
+
+router.delete('/:id', (req, res) => {
+    const{id} = req.params;
+
+    //filter works such that it returns a new array with all the elements that pass the test (true)
+    //if we are to delete a user we want to iterate through all the current users and make sure that the user we want to delete fails the filter test 
+    //we can generate a False as (!True) by doing user.id !== is
+    //if our id was 3 and we iterate over a ex users array being [2,6,1,4,3,7] then we would get [2,6,1,4,7] bc when we iterate over 3 it fails the test as user.id(3) !== 3(we receive from req.params)
+    users = users.filter((user) => user.id !== id);
 });
 
 export default router;
